9a591e5a5e37e172e58815bd7bef99c7a3df3011
xiaoC <2637336491@qq.com>
2026-05-14 10:18:09 +0800

    移除不必要文件

--- MDK/output/debug/ring_buf.d
+++
@@ -1,8 +0,0 @@
-./output/debug/ring_buf.o: ..\source\ring_buf.c ..\source\ring_buf.h \
-  ..\drivers\hc32_ll_driver\inc\hc32_ll_def.h \
-  ..\drivers\hc32_ll_driver\inc\hc32_ll_utility.h \
-  ..\drivers\cmsis\Device\HDSC\hc32f4xx\Include\hc32f4xx.h \
-  ..\drivers\cmsis\Device\HDSC\hc32f4xx\Include\hc32f460.h \
-  ..\drivers\cmsis\Include\core_cm4.h \
-  ..\drivers\cmsis\Device\HDSC\hc32f4xx\Include\system_hc32f460.h \
-  ..\source\hc32f4xx_conf.h
